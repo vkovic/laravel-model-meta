f9f0c6e2a4fe40fc18d085e1153821b17878f767
Vladimir Ković <vlada.kovic@gmail.com>
2018-12-17 13:28:26 +0100

    Using same migration file as vkovic/laravel-meta properly

--- src/package/Providers/LaravelModelMetaServiceProvider.php
+++ src/package/Providers/LaravelModelMetaServiceProvider.php
@@ -16,9 +16,27 @@ class LaravelModelMetaServiceProvider extends ServiceProvider
         // This package depends on vkovic/laravel-meta package
         // and it's migration, so we'll load it here.
         // If user already have it installed, this migration will do nothing
-        $this->loadMigrationsFrom(__DIR__ . '/../../../vendor/vkovic/laravel-meta/src/database/migrations');
+        $this->loadMigrationsFrom($this->vendorDir('vkovic/laravel-meta/src/database/migrations'));
 
         // Load this package migrations
         $this->loadMigrationsFrom(__DIR__ . '/../../database/migrations');
+    }
+
+    /**
+     * Get vendor dir
+     *
+     * @param $packageDir
+     *
+     * @return string
+     */
+    protected function vendorDir($packageDir = '')
+    {
+        if (strpos(app_path(), 'vendor/orchestra/testbench-core') !== false) {
+            // Package context
+            return __DIR__ . '/../../../vendor/' . ltrim($packageDir, '/');
+        } else {
+            // Laravel app context
+            return __DIR__ . '/../../../../../../vendor/' . ltrim($packageDir, '/');
+        }
     }
 }
